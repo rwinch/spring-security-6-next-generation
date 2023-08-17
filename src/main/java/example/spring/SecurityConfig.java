9b95aa34916c38e490c7fd9712d50185c2b69111
Rob Winch <rwinch@users.noreply.github.com>
2023-08-17 16:57:10 -0500

    @Configuration

--- src/main/java/example/spring/SecurityConfig.java
+++ src/main/java/example/spring/SecurityConfig.java
@@ -1,6 +1,7 @@
 package example.spring;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,6 +10,7 @@ import org.springframework.security.web.DefaultSecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebSecurity
+@Configuration(proxyBeanMethods = false)
 public class SecurityConfig {
 	@Bean
 	DefaultSecurityFilterChain springSecurity(HttpSecurity http) throws Exception {
