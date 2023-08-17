e82782d69a08cf1b71f020afab9963b6ec27d241
Rob Winch <rwinch@users.noreply.github.com>
2023-08-17 16:56:33 -0500

    Use lambda

--- src/main/java/example/spring/SecurityConfig.java
+++ src/main/java/example/spring/SecurityConfig.java
@@ -1,20 +1,24 @@
 package example.spring;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebSecurity
 public class SecurityConfig {
 	@Bean
 	DefaultSecurityFilterChain springSecurity(HttpSecurity http) throws Exception {
 		http
-			.authorizeHttpRequests()
+			.authorizeHttpRequests(requests -> requests
 				.requestMatchers("/user").authenticated()
-				.and()
-			.formLogin().and()
-			.httpBasic();
+				.anyRequest().permitAll()
+			)
+			.formLogin(withDefaults())
+			.httpBasic(withDefaults());
 		return http.build();
 	}
 }
