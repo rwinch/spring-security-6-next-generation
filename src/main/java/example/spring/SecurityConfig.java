2ed1927a7e744c33304154be1ca2ab738fc5a63c
Rob Winch <rwinch@users.noreply.github.com>
2023-08-17 17:12:31 -0500

    BREACH

--- src/main/java/example/spring/SecurityConfig.java
+++ src/main/java/example/spring/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.csrf.XorCsrfTokenRequestAttributeHandler;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -14,7 +15,13 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class SecurityConfig {
 	@Bean
 	DefaultSecurityFilterChain springSecurity(HttpSecurity http) throws Exception {
+		XorCsrfTokenRequestAttributeHandler requestHandler = new XorCsrfTokenRequestAttributeHandler();
+		// set the name of the attribute the CsrfToken will be populated on
+		requestHandler.setCsrfRequestAttributeName("_csrf");
 		http
+			.csrf(csrf -> csrf
+				.csrfTokenRequestHandler(requestHandler)
+			)
 			.authorizeHttpRequests(requests -> requests
 				.requestMatchers("/user").authenticated()
 				.anyRequest().permitAll()
