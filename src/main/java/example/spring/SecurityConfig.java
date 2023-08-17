767befd04d04e36bc688165f2d2f2230844e7f7b
Rob Winch <rwinch@users.noreply.github.com>
2023-08-17 14:20:11 -0500

    Remove WebSecurityConfigurerAdapter

--- src/main/java/example/spring/SecurityConfig.java
+++ src/main/java/example/spring/SecurityConfig.java
@@ -1,18 +1,20 @@
 package example.spring;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.DefaultSecurityFilterChain;
 
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+public class SecurityConfig {
+	@Bean
+	DefaultSecurityFilterChain springSecurity(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
 				.antMatchers("/user").authenticated()
 				.and()
 			.formLogin().and()
 			.httpBasic();
+		return http.build();
 	}
 }
